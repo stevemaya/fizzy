c72316d01c83e70403e58c14d0f10112956b1023
Steve Maya <steve.maya@codeimmersives.com>
2020-09-24 11:56:09 -0500

    finished it

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 function fizzy(num) {
-
+  if (num % 3 === 0 && num % 5 === 0)
+    return "FizzBuzz"; else if (num % 3 === 0) {
+      return "Fizz";
+    } else if (num % 5 === 0) {
+      return "Buzz";
+    } else {
+      return num;
+  }
 }
 
 module.exports = {
